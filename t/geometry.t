Marirossibn/PrusaSlicer

commit b76f222d029ac541aa607bab873453cf55abccb2

Removed dependency on Math::Clipper

lib/Slic3r/Geometry.pm
@@ -792,14 +792,13 @@ sub chained_path {
     my %indices = map { $points[$_] => $_ } 0 .. $#points;
     
     my @result = ();
-    my $last_point;
-    if (!$start_near) {
+    if (!$start_near && @points) {
         $start_near = shift @points;
-        push @result, $indices{$start_near} if $start_near;
+        push @result, $indices{$start_near};
     }
     while (@points) {
-        $start_near = $start_near->nearest_point(\@points);
-        @points = grep $_ ne $start_near, @points;
+        my $idx = $start_near->nearest_point_index(\@points);
+        my ($start_near) = splice @points, $idx, 1;
         push @result, $indices{$start_near};
     }
     

t/geometry.t
@@ -177,7 +177,7 @@ is Slic3r::Geometry::can_connect_points(@$points, $polygons), 0, 'can_connect_po
 #==========================================================
 
 {
-    my $bb = Slic3r::Geometry::BoundingBox->new_from_points([ [0, 1], [10, 2], [20, 2] ]);
+    my $bb = Slic3r::Geometry::BoundingBox->new_from_points([ map Slic3r::Point->new(@$_), [0, 1], [10, 2], [20, 2] ]);
     $bb->scale(2);
     is_deeply $bb->extents, [ [0,40], [2,4] ], 'bounding box is scaled correctly';
 }

Build.PL
@@ -12,7 +12,6 @@ my %prereqs = qw(
     File::Basename                  0
     File::Spec                      0
     Getopt::Long                    0
-    Math::Clipper                   1.22
     Math::ConvexHull::MonotoneChain 0.01
     Math::Geometry::Voronoi         1.3
     Math::PlanePath                 53

lib/Slic3r/ExPolygon.pm
@@ -8,7 +8,7 @@ use Boost::Geometry::Utils;
 use List::Util qw(first);
 use Math::Geometry::Voronoi;
 use Slic3r::Geometry qw(X Y A B point_in_polygon same_line epsilon);
-use Slic3r::Geometry::Clipper qw(union_ex JT_MITER);
+use Slic3r::Geometry::Clipper qw(union_ex);
 
 sub wkt {
     my $self = shift;

lib/Slic3r/Fill/Concentric.pm
@@ -4,7 +4,7 @@ use Moo;
 extends 'Slic3r::Fill::Base';
 
 use Slic3r::Geometry qw(scale unscale X);
-use Slic3r::Geometry::Clipper qw(offset offset2 union_pt traverse_pt PFT_EVENODD);
+use Slic3r::Geometry::Clipper qw(offset offset2 union_pt traverse_pt);
 
 sub fill_surface {
     my $self = shift;

lib/Slic3r/GCode/MotionPlanner.pm
@@ -14,7 +14,7 @@ has '_tolerance'    => (is => 'lazy');
 
 use List::Util qw(first);
 use Slic3r::Geometry qw(A B scale epsilon);
-use Slic3r::Geometry::Clipper qw(diff_ex offset JT_MITER);
+use Slic3r::Geometry::Clipper qw(diff_ex offset);
 
 # clearance (in mm) from the perimeters
 has '_inner_margin' => (is => 'ro', default => sub { scale 0.5 });

lib/Slic3r/GUI/Plater.pm
@@ -5,7 +5,7 @@ use utf8;
 
 use File::Basename qw(basename dirname);
 use List::Util qw(max sum first);
-use Math::Clipper qw(offset JT_ROUND);
+use Slic3r::Geometry::Clipper qw(offset JT_ROUND);
 use Math::ConvexHull::MonotoneChain qw(convex_hull);
 use Slic3r::Geometry qw(X Y Z MIN MAX);
 use threads::shared qw(shared_clone);
@@ -681,7 +681,6 @@ sub export_gcode {
 sub export_gcode2 {
     my $self = shift;
     my ($print, $output_file, %params) = @_;
-    $Slic3r::Geometry::Clipper::clipper = Math::Clipper->new;
     local $SIG{'KILL'} = sub {
         Slic3r::debugf "Exporting cancelled; exiting thread...\n";
         threads->exit();
@@ -822,7 +821,6 @@ sub make_thumbnail {
     my $object = $self->{objects}[$obj_idx];
     $object->thumbnail_scaling_factor($self->{scaling_factor});
     my $cb = sub {
-    	$Slic3r::Geometry::Clipper::clipper = Math::Clipper->new if $Slic3r::have_threads;
         my $thumbnail = $object->make_thumbnail;
         
         if ($Slic3r::have_threads) {

lib/Slic3r/Geometry/Clipper.pm
@@ -5,13 +5,11 @@ use warnings;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(offset offset_ex
-    diff_ex diff union_ex intersection_ex xor_ex PFT_EVENODD JT_MITER JT_ROUND
+    diff_ex diff union_ex intersection_ex xor_ex JT_ROUND
     JT_SQUARE is_counter_clockwise union_pt offset2 offset2_ex traverse_pt
     intersection union);
 
-use Math::Clipper 1.22 qw(:cliptypes :polyfilltypes :jointypes is_counter_clockwise area);
 use Slic3r::Geometry qw(scale);
-our $clipper = Math::Clipper->new;
 
 sub traverse_pt {
     my ($polynodes) = @_;

commit 66cac278b970689955f005e5a91564ec75e9f183

Fix regression in chained_path() introduced in b76f222d029ac541aa607bab873453cf55abccb2. Includes regression test. #1184

lib/Slic3r/Geometry.pm
@@ -800,7 +800,7 @@ sub chained_path {
     }
     while (@points) {
         my $idx = $start_near->nearest_point_index(\@points);
-        my ($start_near) = splice @points, $idx, 1;
+        ($start_near) = splice @points, $idx, 1;
         push @result, $indices{$start_near};
     }
     

t/geometry.t
@@ -2,7 +2,7 @@ use Test::More;
 use strict;
 use warnings;
 
-plan tests => 25;
+plan tests => 26;
 
 BEGIN {
     use FindBin;
@@ -12,7 +12,8 @@ BEGIN {
 use Slic3r;
 use Slic3r::Geometry qw(PI polyline_remove_parallel_continuous_edges 
     polyline_remove_acute_vertices polygon_remove_acute_vertices
-    polygon_remove_parallel_continuous_edges polygon_is_convex);
+    polygon_remove_parallel_continuous_edges polygon_is_convex
+    chained_path_points epsilon scale);
 
 #==========================================================
 
@@ -187,6 +188,16 @@ is Slic3r::Geometry::can_connect_points(@$points, $polygons), 0, 'can_connect_po
 {
     my $line = Slic3r::Line->new([10,10], [20,10]);
     is +($line->grow(5))[0]->area, Slic3r::Polygon->new([5,5], [25,5], [25,15], [5,15])->area, 'grow line';
+}
+
+#==========================================================
+
+{
+    # if chained_path() works correctly, these points should be joined with no diagonal paths
+    # (thus 26 units long)
+    my @points = map Slic3r::Point->new_scale(@$_), [26,26],[52,26],[0,26],[26,52],[26,0],[0,52],[52,52],[52,0];
+    my @ordered = @{chained_path_points(\@points, $points[0])};
+    ok !(grep { abs($ordered[$_]->distance_to($ordered[$_+1]) - scale 26) > epsilon } 0..$#ordered-1), 'chained_path';
 }
 
 #==========================================================