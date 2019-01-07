Marirossibn/PrusaSlicer

commit 6c8d8e0ed96b76ad4c6a625ec172cd08fb13d445

Replaced all wxString.ToStdString() with wxString.ToUTF8().data() to be sure that the strings are correctly converted to UTF8.

src/slic3r/GUI/Field.cpp
@@ -188,7 +188,7 @@ void Field::get_value_by_opt_type(wxString& str)
             }
         }
     
-        m_value = str.ToStdString();
+        m_value = str.ToUTF8().data();
 		break; }
 	default:
 		break;

src/libslic3r/Config.cpp
@@ -404,7 +404,7 @@ void ConfigBase::load_from_gcode_file(const std::string &file)
 
     size_t key_value_pairs = load_from_gcode_string(data.data());
     if (key_value_pairs < 80)
-        throw std::runtime_error((boost::format("Suspiciously low number of configuration values extracted from %1: %2") % file % key_value_pairs).str());
+        throw std::runtime_error((boost::format("Suspiciously low number of configuration values extracted from %1%: %2%") % file % key_value_pairs).str());
 }
 
 // Load the config keys from the given string.
@@ -536,7 +536,7 @@ void DynamicConfig::read_cli(const std::vector<std::string> &tokens, t_config_op
     args.emplace_back(const_cast<char*>(""));
     for (size_t i = 0; i < tokens.size(); ++ i)
         args.emplace_back(const_cast<char *>(tokens[i].c_str()));
-    this->read_cli(args.size(), &args[0], extra);
+    this->read_cli(int(args.size()), &args[0], extra);
 }
 
 bool DynamicConfig::read_cli(int argc, char** argv, t_config_option_keys* extra)

src/slic3r/GUI/BackgroundSlicingProcess.cpp
@@ -126,7 +126,7 @@ public:
     }
 
     inline std::string get_name() const {
-        return fpath.GetName().ToStdString();
+        return fpath.GetName().ToUTF8().data();
     }
 
     template<class T> inline LayerWriter& operator<<(const T& arg) {

src/slic3r/GUI/BedShapeDialog.cpp
@@ -305,7 +305,7 @@ void BedShapePanel::load_stl()
 	dialog->GetPaths(input_file);
 	dialog->Destroy();
 
-	std::string file_name = input_file[0].ToStdString();
+	std::string file_name = input_file[0].ToUTF8().data();
 
 	Model model;
 	try {

src/slic3r/GUI/ConfigWizard.cpp
@@ -72,8 +72,7 @@ PrinterPicker::PrinterPicker(wxWindow *parent, const VendorProfile &vendor, cons
 	std::vector<wxPanel*> variants_panels;
 
 	for (const auto &model : models) {
-		auto bitmap_file = wxString::Format("printers/%s_%s.png", vendor.id, model.id);
-		wxBitmap bitmap(GUI::from_u8(Slic3r::var(bitmap_file.ToStdString())), wxBITMAP_TYPE_PNG);
+		wxBitmap bitmap(GUI::from_u8(Slic3r::var((boost::format("printers/%1%_%2%.png") % vendor.id % model.id).str())), wxBITMAP_TYPE_PNG);
 
 		auto *title = new wxStaticText(this, wxID_ANY, model.name, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
 		title->SetFont(namefont);

src/slic3r/GUI/GUI_App.cpp
@@ -509,7 +509,7 @@ void GUI_App::save_language()
     if (m_wxLocale)
         language = m_wxLocale->GetCanonicalName();
 
-    app_config->set("translation_language", language.ToStdString());
+    app_config->set("translation_language", language.ToUTF8().data());
     app_config->save();
 }
 

src/slic3r/GUI/GUI_ObjectList.cpp
@@ -298,13 +298,13 @@ void ObjectList::update_name_in_model(const wxDataViewItem& item)
     if (obj_idx < 0) return;
 
     if (m_objects_model->GetParent(item) == wxDataViewItem(0)) {
-        (*m_objects)[obj_idx]->name = m_objects_model->GetName(item).ToStdString();
+        (*m_objects)[obj_idx]->name = m_objects_model->GetName(item).ToUTF8().data();
         return;
     }
 
     const int volume_id = m_objects_model->GetVolumeIdByItem(item);
     if (volume_id < 0) return;
-    (*m_objects)[obj_idx]->volumes[volume_id]->name = m_objects_model->GetName(item).ToStdString();
+    (*m_objects)[obj_idx]->volumes[volume_id]->name = m_objects_model->GetName(item).ToUTF8().data();
 }
 
 void ObjectList::init_icons()
@@ -657,7 +657,7 @@ void ObjectList::append_menu_item_add_generic(wxMenuItem* menu, const int type) 
     std::vector<std::string> menu_items = { L("Box"), L("Cylinder"), L("Sphere"), L("Slab") };
     for (auto& item : menu_items) {
         append_menu_item(sub_menu, wxID_ANY, _(item), "",
-            [this, type, item](wxCommandEvent&) { load_generic_subobject(_(item).ToStdString(), type); }, "", menu->GetMenu());
+            [this, type, item](wxCommandEvent&) { load_generic_subobject(_(item).ToUTF8().data(), type); }, "", menu->GetMenu());
     }
 
     menu->SetSubMenu(sub_menu);
@@ -683,10 +683,10 @@ void ObjectList::append_menu_items_add_volume(wxMenu* menu)
         append_menu_item(menu, wxID_ANY, _(L("Add part")), "",
             [this](wxCommandEvent&) { load_subobject(ModelVolume::MODEL_PART); }, *m_bmp_vector[ModelVolume::MODEL_PART]);
         append_menu_item(menu, wxID_ANY, _(L("Add support enforcer")), "",
-            [this](wxCommandEvent&) { load_generic_subobject(_(L("Box")).ToStdString(), ModelVolume::SUPPORT_ENFORCER); }, 
+            [this](wxCommandEvent&) { load_generic_subobject(_(L("Box")).ToUTF8().data(), ModelVolume::SUPPORT_ENFORCER); }, 
             *m_bmp_vector[ModelVolume::SUPPORT_ENFORCER]);
         append_menu_item(menu, wxID_ANY, _(L("Add support blocker")), "",
-            [this](wxCommandEvent&) { load_generic_subobject(_(L("Box")).ToStdString(), ModelVolume::SUPPORT_BLOCKER); }, 
+            [this](wxCommandEvent&) { load_generic_subobject(_(L("Box")).ToUTF8().data(), ModelVolume::SUPPORT_BLOCKER); }, 
             *m_bmp_vector[ModelVolume::SUPPORT_BLOCKER]);
 
         return;
@@ -823,7 +823,7 @@ void ObjectList::load_part( ModelObject* model_object,
     wxArrayString input_files;
     wxGetApp().import_model(parent, input_files);
     for (int i = 0; i < input_files.size(); ++i) {
-        std::string input_file = input_files.Item(i).ToStdString();
+        std::string input_file = input_files.Item(i).ToUTF8().data();
 
         Model model;
         try {

src/slic3r/GUI/MainFrame.cpp
@@ -488,7 +488,7 @@ void MainFrame::quick_slice(const int qs)
 //     });
 
     // keep model around
-    auto model = Slic3r::Model::read_from_file(input_file.ToStdString());
+    auto model = Slic3r::Model::read_from_file(input_file.ToUTF8().data());
 
 //     sprint->apply_config(config);
 //     sprint->set_model(model);
@@ -630,7 +630,7 @@ void MainFrame::export_config()
     if (!file.IsEmpty()) {
         wxGetApp().app_config->update_config_dir(get_dir_name(file));
         m_last_config = file;
-        config.save(file.ToStdString());
+        config.save(file.ToUTF8().data());
     }
 }
 
@@ -649,7 +649,7 @@ void MainFrame::load_config_file(wxString file/* = wxEmptyString*/)
         dlg->Destroy();
     }
     try {
-        wxGetApp().preset_bundle->load_config_file(file.ToStdString()); 
+        wxGetApp().preset_bundle->load_config_file(file.ToUTF8().data()); 
     } catch (const std::exception &ex) {
         show_error(this, ex.what());
         return;
@@ -682,7 +682,7 @@ void MainFrame::export_configbundle()
         // Export the config bundle.
         wxGetApp().app_config->update_config_dir(get_dir_name(file));
         try {
-            wxGetApp().preset_bundle->export_configbundle(file.ToStdString()); 
+            wxGetApp().preset_bundle->export_configbundle(file.ToUTF8().data()); 
         } catch (const std::exception &ex) {
 			show_error(this, ex.what());
         }
@@ -710,7 +710,7 @@ void MainFrame::load_configbundle(wxString file/* = wxEmptyString, const bool re
 
     auto presets_imported = 0;
     try {
-        presets_imported = wxGetApp().preset_bundle->load_configbundle(file.ToStdString());
+        presets_imported = wxGetApp().preset_bundle->load_configbundle(file.ToUTF8().data());
     } catch (const std::exception &ex) {
         show_error(this, ex.what());
         return;

commit ff2d8da1132b4fe19145fb40855593086698b8b0

Fix of #1598, #1601, #1604, #1612 The text fields were not causing config value updates,
broken with 6c8d8e0ed96b76ad4c6a625ec172cd08fb13d445
Replaced all wxString.ToStdString() with wxString.ToUTF8().data()
to be sure that the strings are correctly converted to UTF8.

src/slic3r/GUI/Field.cpp
@@ -188,7 +188,7 @@ void Field::get_value_by_opt_type(wxString& str)
             }
         }
     
-        m_value = str.ToUTF8().data();
+        m_value = std::string(str.ToUTF8().data());
 		break; }
 	default:
 		break;