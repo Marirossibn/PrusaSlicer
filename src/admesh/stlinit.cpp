Marirossibn/PrusaSlicer

commit 2d13421cbe4d82d9822e97d2fa2956b5fe68d5a9

The ASCII STL import was made more tolerant to non-standard input format, namely: 1) Whitespaces are allowed at the end of lines 2) A freeform is allowed after "endloop", "endfacet" and "endsolid", if separated from the keyword by a whitespace. Some ASCII STL exporters likely add their annotations in there.
Fixes "Error on importing stl" #2813

src/admesh/stlinit.cpp
@@ -162,23 +162,23 @@ static bool stl_read(stl_file *stl, FILE *fp, int first_facet, bool first)
 			// Read a single facet from an ASCII .STL file
 			// skip solid/endsolid
 			// (in this order, otherwise it won't work when they are paired in the middle of a file)
-			fscanf(fp, "endsolid%*[^\n]\n");
-			fscanf(fp, "solid%*[^\n]\n");  // name might contain spaces so %*s doesn't work and it also can be empty (just "solid")
+			fscanf(fp, "endsolid %*[^\n]\n");
+			fscanf(fp, "solid %*[^\n]\n");  // name might contain spaces so %*s doesn't work and it also can be empty (just "solid")
 			// Leading space in the fscanf format skips all leading white spaces including numerous new lines and tabs.
-			int res_normal     = fscanf(fp, " facet normal %31s %31s %31s", normal_buf[0], normal_buf[1], normal_buf[2]);
+			int res_normal     = fscanf(fp, " facet normal %31s %31s %31s ", normal_buf[0], normal_buf[1], normal_buf[2]);
 			assert(res_normal == 3);
-			int res_outer_loop = fscanf(fp, " outer loop");
+			int res_outer_loop = fscanf(fp, " outer loop ");
 			assert(res_outer_loop == 0);
-			int res_vertex1    = fscanf(fp, " vertex %f %f %f", &facet.vertex[0](0), &facet.vertex[0](1), &facet.vertex[0](2));
+			int res_vertex1    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[0](0), &facet.vertex[0](1), &facet.vertex[0](2));
 			assert(res_vertex1 == 3);
-			int res_vertex2    = fscanf(fp, " vertex %f %f %f", &facet.vertex[1](0), &facet.vertex[1](1), &facet.vertex[1](2));
+			int res_vertex2    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[1](0), &facet.vertex[1](1), &facet.vertex[1](2));
 			assert(res_vertex2 == 3);
-			int res_vertex3    = fscanf(fp, " vertex %f %f %f", &facet.vertex[2](0), &facet.vertex[2](1), &facet.vertex[2](2));
+			int res_vertex3    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[2](0), &facet.vertex[2](1), &facet.vertex[2](2));
 			assert(res_vertex3 == 3);
-			int res_endloop    = fscanf(fp, " endloop");
+			int res_endloop    = fscanf(fp, " endloop %*[^\n]\n");
 			assert(res_endloop == 0);
 			// There is a leading and trailing white space around endfacet to eat up all leading and trailing white spaces including numerous tabs and new lines.
-			int res_endfacet   = fscanf(fp, " endfacet ");
+			int res_endfacet   = fscanf(fp, " endfacet %*[^\n]\n");
 			if (res_normal != 3 || res_outer_loop != 0 || res_vertex1 != 3 || res_vertex2 != 3 || res_vertex3 != 3 || res_endloop != 0 || res_endfacet != 0) {
 				BOOST_LOG_TRIVIAL(error) << "Something is syntactically very wrong with this ASCII STL! ";
 				return false;

commit be887fcfda42f6b23fad3d16a4ea71b999bb5e13

Fighting the ASCII STL import. Fix of 2.1.0-rc: Loading MMU STL's results in object placement off the plater (#2868) It has been broken with 2d13421cbe4d82d9822e97d2fa2956b5fe68d5a9 when trying to fix "Error on importing stl" #2813

src/admesh/stlinit.cpp
@@ -162,24 +162,31 @@ static bool stl_read(stl_file *stl, FILE *fp, int first_facet, bool first)
 			// Read a single facet from an ASCII .STL file
 			// skip solid/endsolid
 			// (in this order, otherwise it won't work when they are paired in the middle of a file)
-			fscanf(fp, "endsolid %*[^\n]\n");
-			fscanf(fp, "solid %*[^\n]\n");  // name might contain spaces so %*s doesn't work and it also can be empty (just "solid")
+			fscanf(fp, " endsolid%*[^\n]\n");
+			fscanf(fp, " solid%*[^\n]\n");  // name might contain spaces so %*s doesn't work and it also can be empty (just "solid")
 			// Leading space in the fscanf format skips all leading white spaces including numerous new lines and tabs.
-			int res_normal     = fscanf(fp, " facet normal %31s %31s %31s ", normal_buf[0], normal_buf[1], normal_buf[2]);
+			int res_normal     = fscanf(fp, " facet normal %31s %31s %31s", normal_buf[0], normal_buf[1], normal_buf[2]);
 			assert(res_normal == 3);
-			int res_outer_loop = fscanf(fp, " outer loop ");
+			int res_outer_loop = fscanf(fp, " outer loop");
 			assert(res_outer_loop == 0);
-			int res_vertex1    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[0](0), &facet.vertex[0](1), &facet.vertex[0](2));
+			int res_vertex1    = fscanf(fp, " vertex %f %f %f", &facet.vertex[0](0), &facet.vertex[0](1), &facet.vertex[0](2));
 			assert(res_vertex1 == 3);
-			int res_vertex2    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[1](0), &facet.vertex[1](1), &facet.vertex[1](2));
+			int res_vertex2    = fscanf(fp, " vertex %f %f %f", &facet.vertex[1](0), &facet.vertex[1](1), &facet.vertex[1](2));
 			assert(res_vertex2 == 3);
+			// Trailing whitespace is there to eat all whitespaces and empty lines up to the next non-whitespace.
 			int res_vertex3    = fscanf(fp, " vertex %f %f %f ", &facet.vertex[2](0), &facet.vertex[2](1), &facet.vertex[2](2));
 			assert(res_vertex3 == 3);
-			int res_endloop    = fscanf(fp, " endloop %*[^\n]\n");
-			assert(res_endloop == 0);
-			// There is a leading and trailing white space around endfacet to eat up all leading and trailing white spaces including numerous tabs and new lines.
-			int res_endfacet   = fscanf(fp, " endfacet %*[^\n]\n");
-			if (res_normal != 3 || res_outer_loop != 0 || res_vertex1 != 3 || res_vertex2 != 3 || res_vertex3 != 3 || res_endloop != 0 || res_endfacet != 0) {
+			// Some G-code generators tend to produce text after "endloop" and "endfacet". Just ignore it.
+			char buf[2048];
+			fgets(buf, 2047, fp);
+			bool endloop_ok = strncmp(buf, "endloop", 7) == 0 && (buf[7] == '\n' || buf[7] == ' ' || buf[7] == '\t');
+			assert(endloop_ok);
+			// Skip the trailing whitespaces and empty lines.
+			fscanf(fp, " ");
+			fgets(buf, 2047, fp);
+			bool endfacet_ok = strncmp(buf, "endfacet", 8) == 0 && (buf[8] == '\n' || buf[8] == ' ' || buf[8] == '\t');
+			assert(endfacet_ok);
+			if (res_normal != 3 || res_outer_loop != 0 || res_vertex1 != 3 || res_vertex2 != 3 || res_vertex3 != 3 || ! endloop_ok || ! endfacet_ok) {
 				BOOST_LOG_TRIVIAL(error) << "Something is syntactically very wrong with this ASCII STL! ";
 				return false;
 			}